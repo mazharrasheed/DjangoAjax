9395e4a86fd94f5658f73d926467cae242946c5a
Mazhar Rasheed <mazharopal@gmail.com>
2024-02-04 14:50:19 +0500

    project completed

--- office/views.py
+++ office/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.core import serializers
 from django.forms.models import model_to_dict
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
@@ -19,20 +21,33 @@ def officecrud(request):
         office=officeform.save()
         return JsonResponse(model_to_dict(office),safe=False)
     
+def change_emp_tojason(employee):
+
+    office=employee.office
+    officejson=model_to_dict(office)
+    response=model_to_dict(employee)
+    response['office']=officejson
+    return response
+
 def employeecrud(request):
     if request.method=='POST':
         officeform=Employeeform(request.POST)
         employee=officeform.save()
-        office=employee.office
-        officejson=model_to_dict(office)
-        response=model_to_dict(employee)
-        response['office']=officejson
+        response=change_emp_tojason(employee)
+        return JsonResponse(response,safe=False)
+    if request.method=='PUT':
+       
+        data=json.loads(request.body)
+        data['office']=Office(id=data.get('office'))
+        employee=Employee(**data)
+        employee.save()
+        response=change_emp_tojason(employee)
         return JsonResponse(response,safe=False)
     
 def getalloffices(request):
     offices=Office.objects.all()
     data=serializers.serialize('json',offices)
-    return JsonResponse(data,safe=False)
+    return JsonResponse({data},safe=False)
 
 def showoffices(request):
     officeform=Officeform()
